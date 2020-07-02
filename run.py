3229af539c3f33209f53e3a17ef57d37d025a8d8
kimanimargaret46@gmail.com <kimanimargaret46@gmail.com>
2020-07-02 18:33:37 +0300

    display users

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3.8
+from user import User
 from credential import Credential
 import random
 def create_user(fname, lname, phone, email):
     """
     Function to create a new user
     """
-    new_user = User(fname, lname, pnumber, email)
+    new_user = User(fname, lname, phone, email)
     return new_user
 
 def create_credential(uname, pword, email):
