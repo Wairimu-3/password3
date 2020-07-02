0c13d9b4fdcf38e41fe05f1d5798d6d89c82380a
kimanimargaret46@gmail.com <kimanimargaret46@gmail.com>
2020-07-02 17:14:56 +0300

    created the credential file and the test file

---
+++ credential.py
@@ -0,0 +1,15 @@
+# credential_array = []
+class Credential:
+    """
+    a class that generates new credential for users
+    """
+    pass
+    credential_array = []
+
+    def __init__(self, user_name, password, email):
+        self.user_name = user_name
+        self.password = password
+        self.email = email
+
+
+
